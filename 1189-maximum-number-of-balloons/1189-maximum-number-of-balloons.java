d26dc2d9a7a503508c19b36e5f6e9f251b104545
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-06-22 11:23:09 +0530

    Time: 2 ms (96.67%), Space: 43.6 MB (54.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxNumberOfBalloons(String s) {
-        int[] freq = new int[26];
-        int count=0;
+        int a=0,b=0,l=0,n=0,o=0;
         for(char c: s.toCharArray()){
-            freq[c-'a']+=1;
-            if(freq[0]>=1 && freq[1]>=1 && freq[11]>=2 && freq[14]>=2 && freq[13]>=1){
-                count+=1;
-                freq[0] -= 1;
-                freq[1] -= 1;
-                freq[11] -= 2;
-                freq[14] -= 2;
-                freq[13] -= 1;
-            }
+            if(c=='a') a+=1;
+            else if(c=='b') b+=1;
+            else if(c=='l') l+=1;
+            else if(c=='n') n+=1;
+            else if(c=='o') o+=1;
         }
-        return count;
+        o/=2;
+        l/=2;
+        return Math.min(a,Math.min(b,Math.min(l,Math.min(o,n))));
     }
 }
