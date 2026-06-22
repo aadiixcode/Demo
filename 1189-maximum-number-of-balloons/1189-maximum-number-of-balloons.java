6d47ec989c1e7dc2b9e3c65e86c10adc215a5c79
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-06-22 10:51:03 +0530

    Time: 20 ms (6.87%), Space: 44 MB (36.65%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,16 +1,24 @@
 class Solution {
     public int maxNumberOfBalloons(String s) {
-        int[] freq = new int[26];
+        Map<Character,Integer> mp = new HashMap<>();
+        mp.put('a',0);
+        mp.put('b',0);
+        mp.put('l',0);
+        mp.put('o',0);
+        mp.put('n',0);
+        
         int count=0;
         for(char c: s.toCharArray()){
-            freq[c-'a']+=1;
-            if(freq[0]>=1 && freq[1]>=1 && freq[11]>=2 && freq[14]>=2 && freq[13]>=1){
+            if(c=='a'||c=='b'||c=='l'||c=='o'||c=='n'){
+                mp.put(c,mp.get(c)+1);
+            }
+            if(mp.get('a')>=1 && mp.get('b')>=1 && mp.get('l')>=2 && mp.get('o')>=2 && mp.get('n')>=1){
                 count+=1;
-                freq[0] -= 1;
-                freq[1] -= 1;
-                freq[11] -= 2;
-                freq[14] -= 2;
-                freq[13] -= 1;
+                mp.put('a',mp.get('a')-1);
+                mp.put('b',mp.get('b')-1);
+                mp.put('l',mp.get('l')-2);
+                mp.put('o',mp.get('o')-2);
+                mp.put('n',mp.get('n')-1);
             }
         }
         return count;
