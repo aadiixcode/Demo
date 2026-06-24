7890c8212f309e96ef80ba397bd35a382b15247e
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-06-24 23:13:21 +0530

    Time: 1 ms (98.83%), Space: 46.3 MB (63.49%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int alternatingSum(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            if(i%2==0){
+            if((i&1)==0){
                 sum += nums[i];
             }
             else{
