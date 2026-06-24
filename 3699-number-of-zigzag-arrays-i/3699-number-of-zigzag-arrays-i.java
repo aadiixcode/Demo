629b557919451788345c21b8aa389148d0f1473d
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-06-24 20:45:28 +0530

    Time: 1612 ms (10.26%), Space: 46.4 MB (87.18%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -1,33 +1,36 @@
 class Solution {
     public int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
-        int[][][] arr = new int[n + 1][m + 1][2];
+        int[][] arr = new int[m + 1][2];
         int mod = 1_000_000_007;
 
+        //base case
         for (int start = 1; start <= m; start++) {
-            arr[n][start][1] = 1;
-            arr[n][start][0] = 1;
+            arr[start][1] = 1;
+            arr[start][0] = 1;
         }
 
         for (int i = n - 1; i > 0; i--) {
             int[] increasingPrefix = new int[m + 1];
             int[] decreasingPrefix = new int[m + 1];
+            int[][] temp = new int[m+1][2];
 
             for (int prev = 1; prev <= m; prev++) {
-                increasingPrefix[prev] = (increasingPrefix[prev - 1] + arr[i + 1][prev][0]) % mod;
-                decreasingPrefix[prev] = (decreasingPrefix[prev - 1] + arr[i + 1][prev][1]) % mod;
+                increasingPrefix[prev] = (increasingPrefix[prev - 1] + arr[prev][0]) % mod;
+                decreasingPrefix[prev] = (decreasingPrefix[prev - 1] + arr[prev][1]) % mod;
             }
 
             for (int prev = 1; prev <= m; prev++) {
-                arr[i][prev][1] = (increasingPrefix[m] - increasingPrefix[prev] + mod) % mod;
-                arr[i][prev][0] = decreasingPrefix[prev - 1];
+                temp[prev][1] = (increasingPrefix[m] - increasingPrefix[prev] + mod) % mod;
+                temp[prev][0] = decreasingPrefix[prev - 1];
             }
+            arr=temp;
         }
 
         int res = 0;
         for (int start = 1; start <= m; start++) {
-            res = (res + arr[1][start][1]) % mod;
-            res = (res + arr[1][start][0]) % mod;
+            res = (res + arr[start][1]) % mod;
+            res = (res + arr[start][0]) % mod;
         }
         return res;
     }
