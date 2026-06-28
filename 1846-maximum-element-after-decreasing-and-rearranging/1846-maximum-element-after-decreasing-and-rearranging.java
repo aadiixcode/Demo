08691045a811084890db8e497b9d9b55b0c4837b
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-06-28 20:06:04 +0530

    Time: 11 ms (12.08%), Space: 77.3 MB (78.75%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,21 +1,13 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        int max = arr.length;
-        int[] temp = new int[max+1];
-        temp[1] = 1;
-        for(int e: arr){
-            if(e>max){
-                temp[max]+=1;
+        Arrays.sort(arr);
+        arr[0] = 1;
+        int ans = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(Math.abs(arr[i]-arr[i-1])>1){
+                arr[i]=arr[i-1]+1;
             }
-            else {
-                temp[e]+=1;
-            }
-        }
-
-        int ans=1;
-        for(int i=2;i<temp.length;i++){
-            int freq = Math.min(i,ans+temp[i]);
-            ans = Math.max(ans,freq);
+            ans = Math.max(ans,arr[i]);
         }
         return ans;
     }
