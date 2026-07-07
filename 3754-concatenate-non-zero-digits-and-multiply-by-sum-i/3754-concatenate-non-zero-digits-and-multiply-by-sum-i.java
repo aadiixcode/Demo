2a908556d783dbe834cb5d7708843ac564a9b6c5
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-07 09:44:50 +0530

    Time: 2 ms (29.75%), Space: 42.8 MB (34.98%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -3,7 +3,7 @@ class Solution {
         if(n==0) return 0;
 
         String s = String.valueOf(n);
-        long sum = 0;
+        int sum = 0;
         long x = 0;
 
         for(char c: s.toCharArray()){
