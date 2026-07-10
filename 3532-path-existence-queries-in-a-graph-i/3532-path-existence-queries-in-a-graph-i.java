fad0360afa0318ec139bd05d9dcf3e758235d004
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-10 20:31:16 +0530

    Time: 99 ms (5.83%), Space: 291 MB (5.83%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,19 +1,46 @@
 class Solution {
-    public boolean[] pathExistenceQueries(int n, int[] nums, int diff, int[][] queries) {
-        int[] component = new int[n];
-        component[0] = 0;
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1]<=diff){
-                component[i] = component[i-1];
+
+    static ArrayList<ArrayList<Integer>> adj;
+
+    static void dfs(int node, int[] component, int comp) {
+        component[node] = comp;
+        for (int e : adj.get(node)) {
+            if (component[e] == 0) {
+                dfs(e, component, comp);
             }
-            else{
-                component[i] = component[i-1]+1;
+        }
+    }
+
+    public boolean[] pathExistenceQueries(int n, int[] nums, int diff, int[][] queries) {
+
+        adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (Math.abs(nums[i] - nums[i + 1]) <= diff) {
+                adj.get(i).add(i + 1);
+                adj.get(i + 1).add(i);
             }
         }
 
+        // System.out.println(adj);
+
+        int[] component = new int[n];
+        int comp = 1;
+        for (int i = 0; i < n; i++) {
+            if (component[i] == 0) {
+                dfs(i, component, comp);
+                comp += 1;
+            }
+        }
+
+        // System.out.println(component);
+
         boolean[] path = new boolean[queries.length];
-        for(int i=0;i<queries.length;i++){
-            if(component[queries[i][0]]==component[queries[i][1]]){
+        for (int i = 0; i < queries.length; i++) {
+            if (component[queries[i][0]] == component[queries[i][1]]) {
                 path[i] = true;
             }
         }
