21649ab17f84ddc99b68559050e9176d2f9b4e0c
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-20 22:23:34 +0530

    Time: 51 ms (7.34%), Space: 46.5 MB (81.46%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,26 @@
+class Solution {
+    static void dfs(int node,int[] vis,int[][] stones){
+        vis[node]=1;
+
+        for(int i=0;i<stones.length;i++){
+            int r = stones[node][0];
+            int c = stones[node][1];
+            if((vis[i]==0) && (stones[i][0]==r || stones[i][1]==c)){
+                dfs(i,vis,stones);
+            }   
+        }
+    }
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        int totalComp = 0;
+        int[] vis = new int[n];
+
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,vis,stones);
+                totalComp+=1;
+            }
+        }
+        return n-totalComp;
+    }
+}
