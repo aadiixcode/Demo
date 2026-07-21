3099b70c701d0b7a6a12bc15ed3f526e2de0918b
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-21 10:33:27 +0530

    Time: 24 ms (60.05%), Space: 46.3 MB (94.81%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,26 +1,69 @@
 class Solution {
-    static void dfs(int node,int[] vis,int[][] stones){
-        vis[node]=1;
-        int r = stones[node][0];
-        int c = stones[node][1];
+    static class DisjointSet {
+        int[] rank;
+        int[] parent;
 
-        for(int i=0;i<stones.length;i++){
-            if((vis[i]==0) && (stones[i][0]==r || stones[i][1]==c)){
-                dfs(i,vis,stones);
-            }   
-        }
-    }
-    public int removeStones(int[][] stones) {
-        int n = stones.length;
-        int totalComp = 0;
-        int[] vis = new int[n];
-
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,vis,stones);
-                totalComp+=1;
+        DisjointSet(int n) {
+            rank = new int[n];
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
         }
-        return n-totalComp;
+
+        int findUltimateParent(int node) {
+            if (node == parent[node])
+                return node;
+            return parent[node] = findUltimateParent(parent[node]);
+        }
+
+        void unionByRank(int u, int v) {
+            int ultimateU = findUltimateParent(u);
+            int ultimateV = findUltimateParent(v);
+            if (ultimateU == ultimateV)
+                return;
+
+            int rankU = rank[ultimateU];
+            int rankV = rank[ultimateV];
+            if (rankU < rankV)
+                parent[ultimateU] = ultimateV;
+            else if (rankV < rankU)
+                parent[ultimateV] = ultimateU;
+            else {
+                parent[ultimateV] = ultimateU;
+                rank[ultimateU] = rankU + 1;
+            }
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        DisjointSet d = new DisjointSet(n);
+
+        for (int i = 0; i < n; i++) {
+            int r = stones[i][0];
+            int c = stones[i][1];
+
+            for (int j = i + 1; j < n; j++) {
+                if (stones[j][0] == r || stones[j][1] == c) {
+                    int u = i;
+                    int v = j;
+                    int ultimateU = d.findUltimateParent(u);
+                    int ultimateV = d.findUltimateParent(v);
+
+                    if (ultimateU != ultimateV) {
+                        d.unionByRank(u, v);
+                    }
+                }
+            }
+        }
+
+        int totalComp = 0;
+        for (int i = 0; i < n; i++) {
+            if (d.parent[i] == i) {
+                totalComp += 1;
+            }
+        }
+        return n - totalComp;
     }
 }
