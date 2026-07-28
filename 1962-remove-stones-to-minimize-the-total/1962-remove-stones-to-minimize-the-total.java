4f24c89736412a1e48d286e4af0019f53fa87437
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-28 13:20:54 +0530

    Time: 313 ms (98.17%), Space: 99.1 MB (15.65%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -2,21 +2,19 @@ class Solution {
     public int minStoneSum(int[] piles, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
 
+        int totalSum = 0;
         for (int e : piles) {
             pq.add(e);
+            totalSum += e;
         }
 
         while (k != 0) {
             int pile = pq.poll();
             int newPile = (pile+1)/2;
+            totalSum -= (pile - newPile);
             pq.add(newPile);
             k -= 1;
         }
-
-        int remainingStones = 0;
-        while(!pq.isEmpty()){
-            remainingStones += pq.poll();
-        }
-        return remainingStones;
+        return totalSum;
     }
 }
