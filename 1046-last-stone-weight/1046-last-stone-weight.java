94e7fcff350abd711cd8ab792d19e6fb98054307
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-29 22:31:17 +0530

    Time: 2 ms (34.42%), Space: 43.1 MB (15.28%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        for(int e: stones){
+            pq.add(e);
+        }
+
+        while(pq.size() > 1){
+            int x = pq.poll();
+            int y = pq.poll();
+            if(x != y){
+                pq.add(x-y);
+            }
+        }
+
+        if(pq.size()==1){
+            return pq.peek();
+        }
+        return 0;
+    }
+}
