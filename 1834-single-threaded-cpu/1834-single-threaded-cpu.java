86d718fe946d197fa9005355e0c3235038eaef69
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-07-30 10:31:20 +0530

    Time: 174 ms (15.39%), Space: 281.5 MB (5.16%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -11,20 +11,21 @@ class Pair {
 class Solution {
     public int[] getOrder(int[][] tasks) {
         List<List<Integer>> arr = new ArrayList<>();
+        int n = tasks.length;
         for (int i = 0; i < tasks.length; i++) {
             int[] task = tasks[i];
-            List l = new ArrayList<>();
-            l.add(task[0]);
-            l.add(task[1]);
-            l.add(i);
-            arr.add(l);
+            List process = new ArrayList<>();
+            process.add(task[0]);
+            process.add(task[1]);
+            process.add(i);
+            arr.add(process);
         }
         Collections.sort(arr, (a, b) -> {
-            // if (a.get(0) == b.get(0)) {
-                // return a.get(1) - b.get(1);
-            // }
             return a.get(0) - b.get(0);
         });
+
+
+
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
             if (a.processingTime == b.processingTime) {
                 return a.taskNumber - b.taskNumber;
@@ -32,7 +33,6 @@ class Solution {
             return a.processingTime - b.processingTime;
         });
 
-        int n = tasks.length;
         int[] ans = new int[n];
         int index = 0;
 
@@ -57,7 +57,6 @@ class Solution {
             Pair p = pq.poll();
             totalTime += p.processingTime;
             ans[index++] = p.taskNumber;
-
         }
         return ans;
     }
