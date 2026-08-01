8eb69d9a2bf693bb8b7567103d492470736acaed
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-01 14:49:05 +0530

    Time: 34 ms (8.03%), Space: 50.6 MB (84.84%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,15 +1,57 @@
 class Solution {
+    static class Pair {
+        int value, row, col;
+
+        Pair(int value, int row, int col) {
+            this.value = value;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int[] sortedArr = new int[n*n];
+        Set<Integer> vis = new HashSet<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.value - b.value);
+        vis.add(0);
+        pq.add(new Pair(matrix[0][0], 0, 0));
 
-        int ind = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                sortedArr[ind++] = matrix[i][j];
+        int ans = matrix[0][0];
+        while (k > 0 && !pq.isEmpty()) {
+            Pair p = pq.poll();
+            int row = p.row;
+            int col = p.col;
+            int value = p.value;
+            k -= 1;
+
+            if(k==0){
+                ans = value;
+                break;
+            }
+
+            if (row + 1 < n) {
+                int key = (n * (row + 1)) + col;
+                if (!vis.contains(key)) {
+                    pq.add(new Pair(matrix[row + 1][col], row + 1, col));
+                    vis.add(key);
+                }
+            }
+            if (col + 1 < n) {
+                int key = (n * row) + (col + 1);
+                if (!vis.contains(key)) {
+                    pq.add(new Pair(matrix[row][col + 1], row, col + 1));
+                    vis.add(key);
+                }
             }
         }
-        Arrays.sort(sortedArr);
-        return sortedArr[k-1];
+        return ans;
     }
 }
+
+/**
+
+1  5  9
+10 11 13
+12 13 15
+
+ */
