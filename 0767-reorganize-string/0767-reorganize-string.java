7455707d00d2d18b0cd08c3227b3ab5669140d67
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-01 22:29:00 +0530

    Time: 3 ms (76.63%), Space: 42.3 MB (99.54%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,40 +1,56 @@
 class Solution {
+    static class Pair {
+        char c;
+        int freq;
+        Pair(char c, int freq) {
+            this.c = c;
+            this.freq = freq;
+        }
+    }
+
     public String reorganizeString(String s) {
         int n = s.length();
         int[] freq = new int[26];
 
-        int maxFreq = 0;
-        char maxFreqChar = 'a';
         for (char c : s.toCharArray()) {
             freq[c-'a'] += 1;
             if(freq[c-'a'] > (n+1)/2){
                 return "";
             }
-            if(freq[c-'a'] > maxFreq){
-                maxFreq = freq[c-'a'];
-                maxFreqChar = c;
-            }
         }
 
-        char[] arr = new char[n];
-        int index = 0;
-        while(freq[maxFreqChar-'a'] > 0){
-           arr[index] = maxFreqChar;
-           index += 2;
-           freq[maxFreqChar-'a'] -= 1;
-        }
-
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
         for(int i=0;i<26;i++){
-            while(freq[i]>0){
-                if(index>=n){
-                    index = 1;
-                }
-                arr[index] = (char) ('a'+i);
-                index += 2;
-                freq[i] -= 1;
+            if(freq[i]>0){
+                char c = (char) ('a'+i);
+                pq.add(new Pair(c,freq[i]));
             }
         }
 
-        return new String(arr);
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() >= 2){
+            Pair p1 = pq.poll();
+            char c1 = p1.c;
+            int f1 = p1.freq;
+            f1 -= 1;
+            sb.append(c1);
+
+            Pair p2 = pq.poll();
+            char c2 = p2.c;
+            int f2 = p2.freq;
+            f2 -= 1;
+            sb.append(c2);
+
+            if(f1 > 0){
+                pq.add(new Pair(c1,f1));
+            }
+            if(f2 > 0){
+                pq.add(new Pair(c2,f2));
+            }
+        }
+        if(!pq.isEmpty()){
+            sb.append(pq.peek().c);
+        }
+        return sb.toString();
     }
 }
