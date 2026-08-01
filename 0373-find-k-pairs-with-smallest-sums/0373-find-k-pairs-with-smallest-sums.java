31945c1accc48ba68aee2c6e350d65059c8eeacb
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-01 13:32:36 +0530

    Time: 58 ms (27.05%), Space: 138.3 MB (50.55%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,41 +1,48 @@
 class Solution {
     static class Pair {
-        int u1, v1, sum;
+        int i, j, sum;
 
-        Pair(int sum, int u1, int v1) {
+        Pair(int sum, int i, int j) {
             this.sum = sum;
-            this.u1 = u1;
-            this.v1 = v1;
+            this.i = i;
+            this.j = j;
         }
     }
 
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.sum - a.sum);
+        int n = nums1.length;
+        int m = nums2.length;
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                int u1 = nums1[i];
-                int v1 = nums2[j];
-                int sum = u1 + v1;
-                if (pq.size() < k) {
-                    pq.add(new Pair(sum,u1,v1));
-                }
-                else if (pq.peek().sum > sum) {
-                    pq.poll();
-                    pq.add(new Pair(sum,u1,v1));
-                } else if (sum >= pq.peek().sum) {
-                    break;
-                }
-            }
-        }
+        Set<Long> vis = new HashSet<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.sum - b.sum);
+        pq.add(new Pair(nums1[0] + nums2[0], 0, 0));
+        vis.add((long) (0));
 
         List<List<Integer>> ans = new ArrayList<>();
-        while (!pq.isEmpty()) {
+        while (ans.size() < k && !pq.isEmpty()) {
             Pair p = pq.poll();
+            int i = p.i;
+            int j = p.j;
+
             List<Integer> temp = new ArrayList<>();
-            temp.add(p.u1);
-            temp.add(p.v1);
+            temp.add(nums1[i]);
+            temp.add(nums2[j]);
             ans.add(temp);
+
+            if (i + 1 < n) {
+                long key = ((long) (i + 1)) * m + j;
+                if (!vis.contains(key)) {
+                    pq.add(new Pair(nums1[i + 1] + nums2[j], i + 1, j));
+                    vis.add(key);
+                }
+            }
+            if (j + 1 < m) {
+                long key = ((long) i) * m + (j + 1);
+                if (!vis.contains(key)) {
+                    pq.add(new Pair(nums1[i] + nums2[j+1], i, j+1));
+                    vis.add(key);
+                }
+            }
         }
         return ans;
     }
