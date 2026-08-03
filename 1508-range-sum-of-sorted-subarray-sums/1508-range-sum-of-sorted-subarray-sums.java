117ee4f8565b5705159627e125ec0d137d2dca36
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-03 18:12:07 +0530

    Time: 341 ms (5.15%), Space: 113.8 MB (5.39%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.java
@@ -1,19 +1,37 @@
 class Solution {
+    static class Pair{
+        int value,index;
+        Pair(int value,int index){
+            this.value = value;
+            this.index = index;
+        }
+    }
     public int rangeSum(int[] nums, int n, int left, int right) {
-        List<Long> list = new ArrayList<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) ->  a.value-b.value);
         for (int i = 0; i < n; i++) {
-            long sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                list.add(sum);
-            }
+            pq.add(new Pair(nums[i],i));
         }
-        Collections.sort(list);
 
-        long sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum = (sum + list.get(i-1)) % 1000000007; 
+        List<Integer> subArrSum = new ArrayList<>();
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            int prevValue = p.value;
+            int prevInd = p.index;
+            subArrSum.add(prevValue);
+
+            if(prevInd+1 < n){
+                int newInd = prevInd + 1;
+                int newValue = prevValue + nums[newInd];
+                pq.add(new Pair(newValue,newInd));
+            }
+        }   
+        System.out.println(subArrSum);     
+
+        long totalSum = 0;
+        for(int i=left;i<=right;i++){
+            totalSum = (totalSum + subArrSum.get(i-1)) % 1000000007;
         }
-        return (int) sum;
+
+        return (int) totalSum;
     }
 }
