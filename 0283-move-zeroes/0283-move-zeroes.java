036246e518bef87b538f07d672c8376c2f8a402e
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-04 14:03:40 +0530

    Time: 2 ms (92.13%), Space: 47.8 MB (38.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
         int zeroStartInd = -1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0 && zeroStartInd != -1){
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == 0) {
+                zeroStartInd = i;
+                break;
+            }
+            i += 1;
+        }
+        while (i < nums.length) {
+            if (nums[i] != 0) {
                 int temp = nums[i];
                 nums[i] = nums[zeroStartInd];
                 nums[zeroStartInd] = temp;
                 zeroStartInd += 1;
             }
-            else if(nums[i]==0 && zeroStartInd == -1){
-                zeroStartInd = i;
-            }
+            i += 1;
         }
     }
 }
