4f932f8674208aabc668f8385ab93e5ddb9a8bf4
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-04 22:47:26 +0530

    Time: 122 ms (67.42%), Space: 105.6 MB (40.21%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        long score = 0;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int e : nums) {
+            maxHeap.add(e);
+        }
+
+        while (k > 0) {
+            int value = maxHeap.poll();
+            score += value;
+
+            double q = (double) value / 3;
+            maxHeap.add((int) Math.ceil(q));
+            k -= 1;
+        }
+        return score;
+    }
+}
