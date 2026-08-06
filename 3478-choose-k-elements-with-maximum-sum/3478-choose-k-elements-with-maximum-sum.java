853320f599769306af3c21d218ad1f0b2afdd5ad
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-06 20:09:00 +0530

    Time: 171 ms (38.46%), Space: 152.3 MB (79.72%) - LeetHub

--- 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.java
+++ 3478-choose-k-elements-with-maximum-sum/3478-choose-k-elements-with-maximum-sum.java
@@ -1,26 +1,32 @@
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
-        int[][] arr = new int[n][3];
-        for(int i = 0; i < n; i++) {
-            arr[i][0] = nums1[i];
-            arr[i][1] = nums2[i];
-            arr[i][2] = i;
-        }
-        Arrays.sort(arr, (a,b) -> a[0] - b[0]);
         long[] ans = new long[n];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[][] arr = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums1[i];
+            arr[i][1] = i;
+            arr[i][2] = nums2[i];
+        }
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         long sum = 0;
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && arr[i - 1][0] == arr[i][0]) {
-               ans[arr[i][2]] = ans[arr[i-1][2]];
+        for (int i = 0; i < n; i++) {
+            int[] p = arr[i];
+            int index = p[1];
+            int nums2Value = p[2];
+
+            if (i > 0 && arr[i - 1][0] == arr[i][0]) {
+                ans[index] = ans[arr[i - 1][1]];
             } else {
-                 ans[arr[i][2]] = sum;
+                ans[index] = sum;
             }
-            pq.add(arr[i][1]);
-            sum += arr[i][1];
-            if(pq.size() > k) {
-                sum -= pq.remove();
+
+            sum += nums2Value;
+            minHeap.add(nums2Value);
+            if (minHeap.size() > k) {
+                sum -= minHeap.poll();
             }
         }
         return ans;
