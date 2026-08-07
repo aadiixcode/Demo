20401bf76d9c4a3b418223cfa37a856ffb204907
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-07 21:31:55 +0530

    Time: 3 ms (99.66%), Space: 273.8 MB (19.32%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,55 +1,29 @@
 class Solution {
-    boolean allBecomesZero(int[] nums, int[][] queries, int k) {
+    public int minZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
         int[] diff = new int[n];
-        for (int i = 0; i <= k; i++) {
-            int low = queries[i][0];
-            int high = queries[i][1];
-            int value = queries[i][2];
-            diff[low] += value;
-            if (high + 1 < n) {
-                diff[high + 1] -= value;
+        int prefixSum = 0;
+        int qIndex = 0;
+        for(int i=0;i<n;i++){
+            prefixSum += diff[i];
+            while(prefixSum < nums[i] && qIndex<queries.length){
+                int[] q = queries[qIndex];
+                int low = q[0];
+                int high = q[1];
+                int value = q[2];
+                diff[low] += value;
+                if(high+1 < n){
+                    diff[high+1] -= value;
+                }
+                if(i>=low && i<=high){
+                    prefixSum += value;
+                }
+                qIndex+=1;
+            }
+            if(prefixSum < nums[i]){
+                return -1;
             }
         }
-
-        for (int i = 1; i < n; i++) {
-            diff[i] += diff[i - 1];
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > diff[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    boolean isAlreadyZero(int[] nums) {
-        for (int e : nums) {
-            if (e != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public int minZeroArray(int[] nums, int[][] queries) {
-        if (isAlreadyZero(nums)) {
-            return 0;
-        }
-
-        int ans = -1;
-        int low = 0, high = queries.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            boolean allZero = allBecomesZero(nums, queries, mid);
-            if (allZero) {
-                ans = mid + 1;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return ans;
+        return qIndex;
     }
 }
