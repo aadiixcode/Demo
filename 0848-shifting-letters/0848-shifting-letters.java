2d42b76763f00750e7775ba15c768b6a901b953b
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-07 12:41:16 +0530

    Time: 13 ms (13.49%), Space: 73.7 MB (25.26%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,20 +1,15 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
         int n = s.length();
-        long[] prefix = new long[n];
-        prefix[n-1] = shifts[n-1];
-        for(int i=n-2;i>=0;i--){
-            prefix[i] = shifts[i] + prefix[i+1];
-        }
-        System.out.println(Arrays.toString(prefix));
-
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
+        long suffix = 0;
+        for(int i=n-1;i>=0;i--){
+            suffix += shifts[i];
             char ch = s.charAt(i);
-            long newPosition = ((ch-'a') + prefix[i]) % 26;
+            long newPosition = ((ch-'a') + suffix) % 26;
             char newChar = (char)('a'+newPosition);
             sb.append(newChar);
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
