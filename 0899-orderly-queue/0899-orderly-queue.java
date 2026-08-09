f52248bb3ce5c3f44676fb1c06f2653d9091a016
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-09 21:42:42 +0530

    Time: 2 ms (99.74%), Space: 44.6 MB (91.82%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.java
+++ 0899-orderly-queue/0899-orderly-queue.java
@@ -7,12 +7,12 @@ class Solution {
         }
 
         String ans = new String(arr);
-        String temp = s + s;
+        String concatenation = s + s;
         int n = s.length();
         for(int i=1;i<=n-1;i++){
-            String t1 = temp.substring(i,n+i);
-            if(t1.compareTo(ans) < 0){
-                ans = t1;
+            String rotatedString = concatenation.substring(i,n+i);
+            if(rotatedString.compareTo(ans) < 0){
+                ans = rotatedString;
             }
         }
         return ans;
