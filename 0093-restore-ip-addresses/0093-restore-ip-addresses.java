91ea568d604c560e7d5579bed31f4218a72ac1e4
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-10 10:38:32 +0530

    Time: 1 ms (99.86%), Space: 43.6 MB (83%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,46 +1,47 @@
 class Solution {
 
-    boolean isValid(String s) {
-        if (s.charAt(0) == '0' || Integer.valueOf(s) > 255) {
-            return false;
-        }
-        return true;
-    }
-
-    void helper(int index, String s, String curr, int parts, List<String> ans) {
+    void helper(int index, String s, StringBuilder curr, int parts, List<String> ans) {
         int n = s.length();
+
         if (index == n || parts == 4) {
             if (index == n && parts == 4) {
-                String ip = new String(curr.substring(0, curr.length() - 1));
-                ans.add(ip);
+                curr.deleteCharAt(curr.length() - 1);
+                ans.add(curr.toString());
+                curr.append(".");
             }
             return;
         }
 
-        if (index + 1 <= n) {
-            String len1 = s.substring(index, index + 1);
-            helper(index + 1, s, curr + len1 + ".", parts + 1, ans);
-        }
-        if (index + 2 <= n) {
-            String len2 = s.substring(index, index + 2);
-            if (isValid(len2)) {
-                helper(index + 2, s, curr + len2 + ".", parts + 1, ans);
+        for (int len = 1; len <= 3 && index + len <= n; len++) {
+
+            String part = s.substring(index, index + len);
+
+            if (len > 1 && part.charAt(0) == '0') {
+                break;
             }
-        }
-        if (index + 3 <= n) {
-            String len3 = s.substring(index, index + 3);
-            if (isValid(len3)) {
-                helper(index + 3, s, curr + len3 + ".", parts + 1, ans);
+
+            if (Integer.parseInt(part) > 255) {
+                break;
             }
+
+            int oldLength = curr.length();
+
+            curr.append(part).append(".");
+            helper(index + len, s, curr, parts + 1, ans);
+
+            curr.setLength(oldLength);
         }
     }
 
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
+
         if (s.length() > 12) {
             return ans;
         }
-        helper(0, s, "", 0, ans);
+
+        helper(0, s, new StringBuilder(), 0, ans);
+
         return ans;
     }
 }
