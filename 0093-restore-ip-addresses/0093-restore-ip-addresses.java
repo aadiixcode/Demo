ead84dd5f13a3e4b4cf353ff05688c75f22405c8
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-10 10:41:42 +0530

    Time: 1 ms (99.86%), Space: 43.8 MB (64.81%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,8 +1,14 @@
 class Solution {
 
+    boolean isValid(String s) {
+        if (s.charAt(0) == '0' || Integer.valueOf(s) > 255) {
+            return false;
+        }
+        return true;
+    }
+
     void helper(int index, String s, StringBuilder curr, int parts, List<String> ans) {
         int n = s.length();
-
         if (index == n || parts == 4) {
             if (index == n && parts == 4) {
                 curr.deleteCharAt(curr.length() - 1);
@@ -15,33 +21,26 @@ class Solution {
         for (int len = 1; len <= 3 && index + len <= n; len++) {
 
             String part = s.substring(index, index + len);
-
             if (len > 1 && part.charAt(0) == '0') {
                 break;
             }
-
             if (Integer.parseInt(part) > 255) {
                 break;
             }
 
             int oldLength = curr.length();
-
             curr.append(part).append(".");
             helper(index + len, s, curr, parts + 1, ans);
-
             curr.setLength(oldLength);
         }
     }
 
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-
         if (s.length() > 12) {
             return ans;
         }
-
         helper(0, s, new StringBuilder(), 0, ans);
-
         return ans;
     }
 }
