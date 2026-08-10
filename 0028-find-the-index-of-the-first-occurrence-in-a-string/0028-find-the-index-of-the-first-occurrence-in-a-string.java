874c6247594fc30a6c9d311edb827bdabf5c3eaa
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-10 14:13:15 +0530

    Time: 1 ms (48.85%), Space: 42.9 MB (82.74%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if (m > n) {
+            return -1;
+        }
+        for (int i = 0; i <= n - m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (s1.charAt(i + j) != s2.charAt(j)) {
+                    break;
+                }
+                if (j == m - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
