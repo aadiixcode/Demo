754e6211ee988bb4205acd42c53ca21051ddba65
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-11 14:13:52 +0530

    Time: 2 ms (57.94%), Space: 43.4 MB (96.84%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -6,10 +6,9 @@ class Solution {
             return false;
         }
 
-        char[] arr = s.toCharArray();
         if (s.equals(goal)) {
             int[] freq = new int[26];
-            for (char ch : arr) {
+            for (char ch : s.toCharArray()) {
                 freq[ch - 'a'] += 1;
                 if (freq[ch - 'a'] > 1) {
                     return true;
@@ -27,12 +26,10 @@ class Solution {
         if (mismatchIndex.size() == 2) {
             int left = mismatchIndex.get(0);
             int right = mismatchIndex.get(1);
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            if(new String(arr).equals(goal)){
+            if (s.charAt(left) == goal.charAt(right) && s.charAt(right) == goal.charAt(left)) {
                 return true;
             }
+            return false;
         }
         return false;
     }
