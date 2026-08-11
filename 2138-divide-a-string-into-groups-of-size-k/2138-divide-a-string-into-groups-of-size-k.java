a97d6c299b89ebe17f747b344a8056a79bb4b15b
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-11 22:46:44 +0530

    Time: 1 ms (99.72%), Space: 43.6 MB (83.66%) - LeetHub

---
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int n = s.length();
+        int numberOfWords = (int) Math.ceil((double) n/k);
+        // System.out.println(numberOfWords);
+        String[] ans = new String[numberOfWords];
+        int ansInd = 0;
+
+        int i = 0;
+        while (i < n) {
+            int j = i;
+            StringBuilder sb = new StringBuilder();
+            while (j < i + k) {
+                if(j<n){
+                    sb.append(s.charAt(j));
+                }
+                else{
+                    sb.append(fill);
+                }
+                j += 1;
+            }
+            ans[ansInd++] = sb.toString();
+            i = j;
+        }
+        return ans;
+    }
+}
