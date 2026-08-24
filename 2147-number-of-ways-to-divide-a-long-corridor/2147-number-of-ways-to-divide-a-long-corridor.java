345618f06484519de3104448a17cba32b035a14d
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-24 22:13:11 +0530

    Time: 31 ms (49.58%), Space: 59.1 MB (21.85%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfWays(String corridor) {
+        int n = corridor.length();
+        int seatCount = 0;
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (corridor.charAt(i) == 'S') {
+                temp.add(i);
+                seatCount += 1;
+            }
+        }
+        if (seatCount == 0 || seatCount % 2 != 0) {
+            return 0;
+        }
+
+        // System.out.println(temp);
+
+        long totalWays = 1;
+        for (int i = 2; i < temp.size(); i += 2) {
+            if (i + 1 < temp.size()) {
+                int size = temp.get(i) - temp.get(i - 1);
+                totalWays = (totalWays * size) % 1000000007;
+            }
+        }
+        return (int)totalWays;
+    }
+}
