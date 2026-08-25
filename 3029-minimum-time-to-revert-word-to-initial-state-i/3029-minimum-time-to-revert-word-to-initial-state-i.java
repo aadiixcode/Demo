cf21a3499e81b0e01c2b1b228039e1cf88a8309d
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-25 20:39:26 +0530

    Time: 1 ms (99.07%), Space: 43.7 MB (42.99%) - LeetHub

---
+++ 3029-minimum-time-to-revert-word-to-initial-state-i/3029-minimum-time-to-revert-word-to-initial-state-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumTimeToInitialState(String word, int k) {
+        int n = word.length();
+        int i = k;
+        int steps = 1;
+        while (i < n) {
+            int start = 0;
+            int j = i;
+            while (j < n && word.charAt(j) == word.charAt(start)) {
+                j += 1;
+                start += 1;
+            }
+            if (j == n) {
+                break;
+            } else {
+                i += k;
+                steps += 1;
+            }
+        }
+        return steps;
+    }
+}
