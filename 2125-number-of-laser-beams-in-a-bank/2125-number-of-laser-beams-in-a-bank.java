9747727b8cbecc7219b458a48a2e66285484bf7e
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-25 08:54:51 +0530

    Time: 12 ms (60.1%), Space: 47.7 MB (55.02%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,28 +1,21 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int m = bank.length;
-        int[] securityCount = new int[m];
-        for (int i = 0; i < m; i++) {
+        int totalLaserBeams = 0;
+        int prev = 0;
+        for (int i = 0; i < bank.length; i++) {
             int count = 0;
             for (char ch : bank[i].toCharArray()) {
                 if (ch == '1') {
                     count += 1;
                 }
             }
-            securityCount[i] = count;
-        }
+            if (count > 0) {
+                if (prev > 0) {
+                    totalLaserBeams += (prev * count);
+                }
+                prev = count;
+            }
 
-        int totalLaserBeams = 0;
-        int i = 0;
-        while (i < m) {
-            int c1 = securityCount[i];
-            i += 1;
-            while (i < m && securityCount[i] == 0) {
-                i += 1;
-            }
-            if (i < m) {
-                totalLaserBeams += (c1 * securityCount[i]);
-            }
         }
         return totalLaserBeams;
     }
