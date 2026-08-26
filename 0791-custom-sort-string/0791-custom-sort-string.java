884d2bd73f0da556f5fd9bc5657a4924ee24cfbf
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-26 22:52:45 +0530

    Time: 5 ms (13.15%), Space: 43.3 MB (12.8%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,24 +1,33 @@
 class Solution {
+    static class Pair {
+        char ch;
+        int position;
+
+        Pair(char ch, int position) {
+            this.ch = ch;
+            this.position = position;
+        }
+
+    }
+
     public String customSortString(String order, String s) {
 
-        int[] freq = new int[26];
-        for(char ch: s.toCharArray()){
-            freq[ch-'a'] += 1;
+        int[] indices = new int[26];
+        Arrays.fill(indices, Integer.MAX_VALUE);
+        for (int i = 0; i < order.length(); i++) {
+            indices[order.charAt(i) - 'a'] = i;
         }
+
+        List<Pair> temp = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            Pair p = new Pair(ch, indices[ch - 'a']);
+            temp.add(p);
+        }
+        Collections.sort(temp, (a, b) -> a.position - b.position);
 
         StringBuilder sb = new StringBuilder();
-        for(char ch: order.toCharArray()){
-            while(freq[ch-'a'] > 0){
-                sb.append(ch);
-                freq[ch-'a']-=1;
-            }
-        }
-
-        for(int i=0;i<26;i++){
-            while(freq[i]>0){
-                sb.append((char)('a'+i));
-                freq[i]-=1;
-            }
+        for (Pair p : temp) {
+            sb.append(p.ch);
         }
         return sb.toString();
     }
