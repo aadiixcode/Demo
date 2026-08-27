5d3d8087b2c1d0025630e0cc6a5c7bc986be07b4
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-27 17:20:02 +0530

    Time: 80 ms (57.7%), Space: 47.8 MB (61.93%) - LeetHub

--- 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
+++ 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
@@ -1,62 +1,49 @@
 class Solution {
-    void findSubstring(String s, List<String> temp) {
+    void findSubstring(String s, Map<String, Integer> mp, List<String> temp) {
+        Set<String> st = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             StringBuilder sb = new StringBuilder();
             for (int j = i; j < s.length(); j++) {
                 sb.append(s.charAt(j));
-                temp.add(sb.toString());
+                String subString = sb.toString();
+                st.add(subString);
+                temp.add(subString);
             }
+        }
+        for (String subString : st) {
+            mp.put(subString, mp.getOrDefault(subString, 0) + 1);
         }
     }
 
     public String[] shortestSubstrings(String[] arr) {
+        Map<String, Integer> mp = new HashMap<>();
         List<String> temp = new ArrayList<>();
-        int n = arr.length;
         for (String s : arr) {
-            findSubstring(s, temp);
+            findSubstring(s, mp, temp);
         }
 
-        // System.out.println(temp);
-
-        String[] answer = new String[n];
-        int index = 0;
-
+        String[] ans = new String[arr.length];
+        int ansIndex = 0;
         int left = 0, right = 0;
         for (String s : arr) {
             int stringSize = s.length();
             left = right;
             right += (stringSize * (stringSize + 1)) / 2;
-            // System.out.println(left + ", " + right);
-            Set<String> st = new HashSet<>();
-            for (int i = 0; i < left; i++) {
-                st.add(temp.get(i));
-            }
-            for (int i = right; i < temp.size(); i++) {
-                st.add(temp.get(i));
-            }
-
-            // System.out.println(st);
-
-            String smallestString = null;
+            String smallerString = "";
             for (int i = left; i < right; i++) {
-                String currentStr = temp.get(i);
-                if (!st.contains(currentStr)) {
-                    if (smallestString == null ||
-                            currentStr.length() < smallestString.length() ||
-                            (currentStr.length() == smallestString.length()
-                                    && currentStr.compareTo(smallestString) < 0)) {
-                        smallestString = currentStr;
+                String subString = temp.get(i);
+                if (mp.get(subString) == 1) {
+                    if (smallerString.equals("")
+                            || subString.length() < smallerString.length()
+                            || (subString.length() == smallerString.length()
+                                    && subString.compareTo(smallerString) < 0)) {
+                        smallerString = subString;
                     }
                 }
             }
-            if (smallestString == null) {
-                smallestString = "";
-            }
-
-            // System.out.println(smallestString);
-            answer[index++] = smallestString;
+            ans[ansIndex++] = smallerString;
         }
-        return answer;
+        return ans;
     }
 }
 
