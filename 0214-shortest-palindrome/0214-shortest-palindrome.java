245475c51a8813210b6666b5b01f1926b2471fc1
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-30 20:38:56 +0530

    Time: 8 ms (62.03%), Space: 47.3 MB (17.52%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,24 +1,40 @@
 class Solution {
+    void findLPS(int[] lps, String s) {
+        int i = 0, j = 1;
+        while (j < s.length()) {
+            if (s.charAt(i) != s.charAt(j)) {
+                if (i != 0) {
+                    i = lps[i - 1];
+                } else {
+                    lps[j] = 0;
+                    j += 1;
+                }
+            } else {
+                i += 1;
+                lps[j] = i;
+                j += 1;
+            }
+        }
+    }
+
     public String shortestPalindrome(String s) {
         int n = s.length();
+        if(n==0){
+            return "";
+        }
         char[] arr = s.toCharArray();
         for (int i = 0; i < n / 2; i++) {
             char temp = arr[i];
             arr[i] = arr[n - i - 1];
             arr[n - 1 - i] = temp;
         }
-
         String reverse = new String(arr);
 
-        int i = 0;
-        while (i < n) {
-            String prefix = s.substring(0, n - i);
-            String suffix = reverse.substring(i, n);
-            if(prefix.equals(suffix)){
-                return reverse.substring(0,i) + s;
-            }
-            i += 1;
-        }
-        return reverse+s;
+        String newString = s + "#" + reverse;
+        int[] lps = new int[newString.length()];
+        findLPS(lps, newString);
+
+        int longestPrefixSuffixMatch = lps[lps.length-1];
+        return reverse.substring(0,n-longestPrefixSuffixMatch) + s;
     }
 }
