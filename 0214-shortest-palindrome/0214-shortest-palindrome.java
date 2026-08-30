87f65fd986f9123e80ebd3c106d237bbe03424b3
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-30 18:14:11 +0530

    Time: 495 ms (5.07%), Space: 46.7 MB (61.31%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - 1 - i] = temp;
+        }
+
+        String reverse = new String(arr);
+
+        int i = 0;
+        while (i <= n) {
+            String prefix = s.substring(0, n - i);
+            String suffix = reverse.substring(i, n);
+            if(prefix.equals(suffix)){
+                return reverse.substring(0,i) + s;
+            }
+            i += 1;
+        }
+        return " ";
+    }
+}
