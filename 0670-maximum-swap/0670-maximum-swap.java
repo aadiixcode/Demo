8af07fd9d8188e4d9717ea28c35f4b79da3db2b8
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-31 09:20:23 +0530

    Time: 0 ms (100%), Space: 42.4 MB (15.51%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -3,26 +3,26 @@ class Solution {
         String s = String.valueOf(num);
         int n = s.length();
 
-        int[] temp = new int[10];
-        Arrays.fill(temp, -1);
-        for (int i = 0; i < n; i++) {
-            temp[s.charAt(i) - '0'] = i;
-        }
-
         char[] charArr = s.toCharArray();
         for (int i = 0; i < n; i++) {
-            int value = charArr[i] - '0';
-            if (value == 9) {
+            int currentValue = charArr[i] - '0';
+            if (currentValue == 9) {
                 continue;
             }
 
-            for (int j = 9; j >= 0; j--) {
-                if (j > value && temp[j] > i) {
-                    char tempChar = charArr[i];
-                    charArr[i] = charArr[temp[j]];
-                    charArr[temp[j]] = tempChar;
-                    return Integer.valueOf(new String(charArr));
+            int maxValueInd = i;
+            for (int j = i + 1; j < n; j++) {
+                int rightValue = charArr[j] - '0';
+                int maxTillNow = charArr[maxValueInd] - '0';
+                if(rightValue >= maxTillNow && rightValue!=currentValue){
+                    maxValueInd = j;
                 }
+            }
+            if(maxValueInd > i){
+                char temp = charArr[i];
+                charArr[i] = charArr[maxValueInd];
+                charArr[maxValueInd] = temp;
+                return Integer.valueOf(new String(charArr));
             }
         }
         return num;
