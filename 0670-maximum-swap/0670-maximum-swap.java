055ef222e757aa70375f6659c82b9a7fb5ccc2aa
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-31 08:56:18 +0530

    Time: 1 ms (68.63%), Space: 42 MB (85.8%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -3,29 +3,28 @@ class Solution {
         String s = String.valueOf(num);
         int n = s.length();
 
-        int[] temp = new int[n];
-        temp[n - 1] = n - 1;
-        int max = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (s.charAt(max) > s.charAt(i)) {
-                temp[i] = max;
-            } else {
-                temp[i] = i;
-                if (s.charAt(i) > s.charAt(max)) {
-                    max = i;
+        int[] temp = new int[10];
+        Arrays.fill(temp, -1);
+        for (int i = 0; i < n; i++) {
+            temp[s.charAt(i) - '0'] = i;
+        }
+
+        char[] charArr = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            int value = charArr[i] - '0';
+            if (value == 9) {
+                continue;
+            }
+
+            for (int j = 9; j >= 0; j--) {
+                if (j > value && temp[j] > i) {
+                    char tempChar = charArr[i];
+                    charArr[i] = charArr[temp[j]];
+                    charArr[temp[j]] = tempChar;
+                    return Integer.valueOf(new String(charArr));
                 }
             }
         }
-
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < n; i++) {
-            if(temp[i] > i){
-               char ch = arr[i];
-               arr[i] = arr[temp[i]];
-               arr[temp[i]] = ch;
-               break;
-            }
-        }
-        return Integer.valueOf(new String(arr));
+        return num;
     }
 }
