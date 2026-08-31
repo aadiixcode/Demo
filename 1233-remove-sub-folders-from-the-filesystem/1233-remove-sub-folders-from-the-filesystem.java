82b8009ff9c5a794c023b7c10fc582430c894510
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-31 11:51:18 +0530

    Time: 44 ms (74.19%), Space: 55.4 MB (49.18%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,30 +1,14 @@
 class Solution {
     public List<String> removeSubfolders(String[] folders) {
         Arrays.sort(folders);
-        Set<String> st = new HashSet<>();
+
         List<String> ans = new ArrayList<>();
-        for (String folder : folders) {
-            if (st.isEmpty()) {
-                st.add(folder);
-                ans.add(folder);
-            } else {
-                int i = folder.length() - 1;
-                boolean isSubfolder = false;
-                while (i >= 0) {
-                    while(i>=0 && folder.charAt(i)!='/'){
-                        i-=1;
-                    }
-                    String parent = folder.substring(0,i);
-                    if(st.contains(parent)){
-                        isSubfolder = true;
-                        break;
-                    }
-                    i -= 1;
-                }
-                if(!isSubfolder){
-                    st.add(folder);
-                    ans.add(folder);
-                }
+        ans.add(folders[0]);
+        for (int i = 1; i < folders.length; i++) {
+            String curr = folders[i];
+            String parent = ans.getLast() + "/";
+            if (curr.indexOf(parent) != 0) {
+                ans.add(curr);
             }
         }
         return ans;
