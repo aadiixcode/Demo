c82f4aba04bdb78a528c3566f4d508e461e74121
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-08-31 22:48:47 +0530

    Time: 9 ms (95.95%), Space: 47.7 MB (43.43%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+        lps[0] = 0;
+        int length = 0;
+        int i = 1;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(length)) {
+                length += 1;
+                lps[i] = length;
+                i += 1;
+            }
+            else {
+                if (length > 0) {
+                    length = lps[length - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i += 1;
+                }
+            }
+        }
+        return s.substring(0,length);
+    }
+}
