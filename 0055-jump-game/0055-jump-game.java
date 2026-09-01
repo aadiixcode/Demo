0ed3f8bb0aba89bc2548b7661ac56e9beaaed025
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-09-01 13:54:05 +0530

    Time: 3 ms (20.59%), Space: 47.3 MB (99.19%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        int maxIndex = 0;
+        for (int i = 0; i < n; i++) {
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+            if (nums[i] == 0 && maxIndex <= i && i != n - 1) {
+                return false;
+            }
+            if (nums[i] != 0 && maxIndex < i) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
