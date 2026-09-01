5cbaad901d4bd185c4f1c6b2c74d4a06c260cf46
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-09-01 11:10:39 +0530

    Time: 25 ms (18.46%), Space: 46.9 MB (33.22%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,19 +1,21 @@
 class Solution {
     public int maximumLength(String s) {
-        Map<Map<Character,Integer>, Integer> mp = new HashMap<>();
+        Map<String, Integer> mp = new HashMap<>();
         int n = s.length();
         int maxLength = -1;
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
+            int length = 0;
             for (int j = i; j < n; j++) {
                 if (s.charAt(i) != s.charAt(j)) {
                     break;
                 }
-                Map<Character,Integer> inner = new HashMap<>();
-                inner.put(ch,j-i+1);
-                mp.put(inner, mp.getOrDefault(inner, 0) + 1);
-                if (mp.get(inner) >= 3) {
-                    maxLength = Math.max(maxLength, j-i+1);
+
+                length += 1;
+                String key = ch + ":" + length;
+                mp.put(key, mp.getOrDefault(key, 0) + 1);
+                if (mp.get(key) >= 3) {
+                    maxLength = Math.max(maxLength, length);
                 }
             }
         }
