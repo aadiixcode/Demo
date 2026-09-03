2917a13531ae5a99b5ac98cfc280005b9a70927a
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-09-03 13:45:46 +0530

    Time: 1 ms (27.84%), Space: 44 MB (6.3%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,24 +1,42 @@
 class Solution {
-    public boolean checkValidString(String s) {
-        int minOpenBracket = 0, maxOpenBracket = 0;
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                minOpenBracket += 1;
-                maxOpenBracket += 1;
-            } else if (ch == ')') {
-                minOpenBracket -= 1;
-                maxOpenBracket -= 1;
-            } else if (ch == '*') {
-                minOpenBracket -= 1;
-                maxOpenBracket += 1;
+    boolean helper(int index, int balance, String s, int[][] dp) {
+        if (index == s.length()) {
+            if (balance == 0) {
+                return true;
             }
-            if (minOpenBracket < 0) {
-                minOpenBracket = 0;
-            }
-            if (maxOpenBracket < 0) {
-                return false;
-            }
+            return false;
         }
-        return minOpenBracket == 0;
+        if (balance < 0) {
+            return false;
+        }
+
+        if (dp[index][balance] != -1) {
+            return dp[index][balance] == 1 ? true : false;
+        }
+
+        boolean isValid = false;
+        if (s.charAt(index) == '(') {
+            isValid |= helper(index + 1, balance + 1, s, dp);
+        } else if (s.charAt(index) == ')') {
+            isValid |= helper(index + 1, balance - 1, s, dp);
+        } else {
+            isValid |= helper(index + 1, balance - 1, s, dp);
+            isValid |= helper(index + 1, balance, s, dp);
+            isValid |= helper(index + 1, balance + 1, s, dp);
+        }
+        if (isValid) {
+            dp[index][balance] = 1;
+            return true;
+        }
+        dp[index][balance] = 0;
+        return false;
+    }
+
+    public boolean checkValidString(String s) {
+        int[][] dp = new int[s.length()][s.length() + 1];
+        for (int i = 0; i < s.length(); i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return helper(0, 0, s, dp);
     }
 }
