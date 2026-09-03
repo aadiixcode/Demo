aabf36f687eef48a18c9c6107be6e30329559cab
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-09-03 14:28:13 +0530

    Time: 1 ms (27.84%), Space: 42.6 MB (83.37%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,42 +1,51 @@
 class Solution {
-    boolean helper(int index, int balance, String s, int[][] dp) {
-        if (index == s.length()) {
-            if (balance == 0) {
-                return true;
-            }
-            return false;
-        }
-        if (balance < 0) {
-            return false;
-        }
-
-        if (dp[index][balance] != -1) {
-            return dp[index][balance] == 1 ? true : false;
-        }
-
-        boolean isValid = false;
-        if (s.charAt(index) == '(') {
-            isValid |= helper(index + 1, balance + 1, s, dp);
-        } else if (s.charAt(index) == ')') {
-            isValid |= helper(index + 1, balance - 1, s, dp);
-        } else {
-            isValid |= helper(index + 1, balance - 1, s, dp);
-            isValid |= helper(index + 1, balance, s, dp);
-            isValid |= helper(index + 1, balance + 1, s, dp);
-        }
-        if (isValid) {
-            dp[index][balance] = 1;
-            return true;
-        }
-        dp[index][balance] = 0;
-        return false;
-    }
 
     public boolean checkValidString(String s) {
-        int[][] dp = new int[s.length()][s.length() + 1];
+        Stack<Integer> bracket = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
-            Arrays.fill(dp[i], -1);
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                bracket.push(i);
+            } else if (ch == ')') {
+                if (!bracket.isEmpty() && s.charAt(bracket.peek()) == '(') {
+                    bracket.pop();
+                } else {
+                    if (!star.isEmpty() && star.peek() < i) {
+                        star.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            } else {
+                star.push(i);
+            }
         }
-        return helper(0, 0, s, dp);
+
+        while(!bracket.isEmpty()){
+            int bracketIndex = bracket.peek();
+            if(s.charAt(bracketIndex) == '('){
+                if (!star.isEmpty() && star.peek() > bracketIndex) {
+                    bracket.pop();
+                    star.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+            else if(s.charAt(bracketIndex) == ')'){
+                if (!star.isEmpty() && star.peek() < bracketIndex) {
+                    bracket.pop();
+                    star.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return bracket.isEmpty();
     }
 }
