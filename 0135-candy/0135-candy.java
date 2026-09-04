dfc29809c079b672ea3f987684932b6115f106c8
aadiixcode <2303031240178@paruluniversity.ac.in>
2026-09-04 22:59:53 +0530

    Time: 3 ms (86.48%), Space: 52.1 MB (26.19%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,16 +3,22 @@ class Solution {
         int n = ratings.length;
         int[] left = new int[n];
         int[] right = new int[n];
-        Arrays.fill(left, 1);
-        Arrays.fill(right, 1);
+
+        left[0] = 1;
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
             }
         }
+
+        right[n - 1] = 1;
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 right[i] = right[i + 1] + 1;
+            } else {
+                right[i] = 1;
             }
         }
 
